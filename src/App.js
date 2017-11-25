dc79a0b182ac94f4c6a9ba826905a301b35c148d
aluna <i>
2017-11-24 18:45:10 -0800

    11/24/17 - Improved error handling using Promise() chaining
    App no longer crashes when bad input is sent to API, added catch() block

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Request from 'superagent';
+import _ from 'lodash';
+//import Weather from './Components/Weather';
+//import WeatherFind from './Components/WeatherFind'
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+
+    this.search()
+
+  }
+
+  search(query = "los angeles") {
+
+    var searchText = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + query + '")';
+    var url = "https://query.yahooapis.com/v1/public/yql?q=" + searchText + "&format=json";
+
+    if (url != null) {
+      Request.get(url).then((response) => { //chain promise, call api and catch error 
+        this.setState({
+          condition: response.body.query.results.channel.item.condition,
+          city: response.body.query.results.channel.location.city
+        });
+        console.log(response.body.query);
+        console.log(response.body.query.results);
+        console.log(response.body.query.results.channel.location.city);
+      }, reason => { 
+        console.log(reason + " --- reason was hit")
+      }).catch((error) =>{ //added catch statement for better error handling 
+
+        console.log(error + "error caught")
+
+      }); //now no errors get thrown while searching api >:)
+    }
+  }
+
+  componentDidMount() {
+    //called after component has been rendered into the page
+  }
+
+  componentWillReceiveProps() {
+    //called when the props provided to the component are changed
+  }
+
+  updateSearch() {
+    this.search(this.refs.query.value);
+  }
+
+  render() { //use lodash on map() for better persformance
+    //const number = [1,2,3,4,5];
+    let weather;
+    let condition = this.state.condition;
+    if (condition) {
+      //i actually have no idea why lodash works :/
+      weather = _.map(condition, (passedInWeather, index) => {
+        return <li key={index}> {passedInWeather} </li>;
+      });
+    }
+    return (
+      <div>
+        Weather App
+        <input ref="query" onChange={(e) => { this.updateSearch(); }} type="text" />
+        <div>
+          
+          {weather}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
